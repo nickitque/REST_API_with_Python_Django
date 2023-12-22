c5854e254cc31f91e317646b92b125f6debde395
Nikita <nickitque.39@gmail.com>
2023-12-22 16:14:46 +0100

    Implemented view for movie details by Primary Key

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -7,5 +7,13 @@ def movie_list(request):
     data = {
         'movies': list(movies.values())
     }
+    return JsonResponse(data)
 
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+        'name': movie.name,
+        'description': movie.description,
+        'active': movie.active,
+    }
     return JsonResponse(data)
