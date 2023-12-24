f7869c3eef482e21795d1cca49605a7f4ce941f7
Nikita <nickitque.39@gmail.com>
2023-12-24 18:10:11 +0100

    Updated api_view decorator method

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,4 +1,17 @@
+from rest_framework.response import Response
 from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+from rest_framework.decorators import api_view
 
+
+@api_view()
 def movie_list(request):
-    pass
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
