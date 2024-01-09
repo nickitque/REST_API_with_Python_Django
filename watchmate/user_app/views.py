4d54cfe5c5157e890d9a3c1fe43ae29c7f84b75e
Nikita <nickitque.39@gmail.com>
2024-01-09 15:00:03 +0100

    Updated urlpatterns, added logout and registrations views

--- watchmate/user_app/views.py
+++ watchmate/user_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from user_app.api.serializers import RegistrationSerializer
 
-# Create your views here.
+@api_view(['POST',])
+def registration_view(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data)
