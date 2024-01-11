c182d3c1ed3d98b5ddf8d042bbde821de6219b49
Nikita <nickitque.39@gmail.com>
2024-01-12 00:58:57 +0100

    Implemented first test case for registration enpoint

--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+#from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data = {
+            "username": "testcase",
+            "email": "testcaseemail@mail.com",
+            "password": "password1",
+            "password2": "password1"
+        }
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
