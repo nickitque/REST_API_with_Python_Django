0ff8c5ba478b2b2e4ba1cceb4552b1f9e72a7c97
Nikita <nickitque.39@gmail.com>
2024-01-12 01:15:17 +0100

    Implemented login and logout testcases

--- watchmate/user_app/tests.py
+++ watchmate/user_app/tests.py
@@ -2,9 +2,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-#from rest_framework.test import APIClient
 from rest_framework.authtoken.models import Token
-
 
 
 class RegisterTestCase(APITestCase):
@@ -18,3 +16,22 @@ class RegisterTestCase(APITestCase):
         }
         response = self.client.post(reverse('register'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginLogoutTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="newPassword123")
+
+    def test_login(self):
+        data = {
+            "username": "example",
+            "password": "newPassword123"
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username="example")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
